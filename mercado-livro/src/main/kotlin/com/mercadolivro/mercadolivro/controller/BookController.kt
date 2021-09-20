159b12af5952bd5b481e69e1173d9465b1269c2b
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2021-09-20 16:16:53 -0300

    add book model, controller

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/controller/BookController.kt
@@ -0,0 +1,22 @@
+package com.mercadolivro.mercadolivro.controller
+
+import com.mercadolivro.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.mercadolivro.extension.toCustomerModel
+import com.mercadolivro.mercadolivro.model.CustomerModel
+import com.mercadolivro.mercadolivro.service.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("book")
+class BookController{
+
+    @PostMapping
+    fun create(@RequestBody request: PostBookRequest){
+
+    }
+
+
+}
---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/model/BookModel.kt
@@ -0,0 +1,30 @@
+package com.mercadolivro.mercadolivro.model
+
+import com.mercadolivro.mercadolivro.enums.BookStatus
+import java.math.BigDecimal
+import java.util.*
+import javax.persistence.*
+
+@Entity(name = "book")
+data class BookModel (
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column(name = "name")
+    var name: String,
+
+    @Column(name = "price")
+    var price: BigDecimal,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    var customer: CustomerModel? = null,
+
+)
