78486e58b227b0432a71d7625954c4436a986964
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2022-01-17 15:49:26 -0300

    add table customer roles, add column password

--- mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/model/CustomerModel.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/model/CustomerModel.kt
@@ -19,5 +19,9 @@ data class CustomerModel (
 
     @Column
     @Enumerated(EnumType.STRING)
-    var status: CustomerStatus
+    var status: CustomerStatus,
+
+    @Column
+    val password: String
+
 )
---
+++ mercado-livro/src/main/resources/db/migration/V6__create_table_customer_roles.sql
@@ -0,0 +1,5 @@
+create table customer_roles(
+	customer_id int not null,
+	roles varchar(50) not null,
+	FOREIGN KEY (customer_id) REFERENCES customer(id)
+)
