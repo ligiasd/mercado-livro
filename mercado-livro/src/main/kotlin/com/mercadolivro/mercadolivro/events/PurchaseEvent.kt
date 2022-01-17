cc10faa84bc4840ab75f50e5ac37c70efd1ad169
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2022-01-17 01:15:23 -0300

    add events soldBookListener, generateNfe

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/events/PurchaseEvent.kt
@@ -0,0 +1,4 @@
+package com.mercadolivro.mercadolivro.events
+
+class PurchaseEvent {
+}
---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/events/listener/GenerateNfeListener.kt
@@ -0,0 +1,18 @@
+package com.mercadolivro.mercadolivro.events
+
+import com.mercadolivro.mercadolivro.service.PurchaseService
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class GenerateNfeListener(
+    private val purchaseService: PurchaseService
+) {
+    @EventListener
+    fun listen(purchaseEvent: PurchaseEvent){
+        val nfe = UUID.randomUUID().toString()
+        val purchaseModel = purchaseEvent.purchaseModel.copy(nfe = nfe)
+        purchaseService.update(purchaseModel)
+    }
+}
