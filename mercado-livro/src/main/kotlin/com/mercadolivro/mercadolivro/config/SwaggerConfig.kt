6ead19749de6a788f89f9d95c5866d186ad73e62
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2022-01-16 23:59:12 -0300

    add entity, migration purchase,purchase_book

--- mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/config/SwaggerConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/mercadolivro/config/SwaggerConfig.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.mercadolivro.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.util.StringUtils
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
@@ -16,11 +17,14 @@ class SwaggerConfig(
         registry.addResourceHandler("$baseUrl/swagger-ui/**")
             .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
             .resourceChain(false)
+
     }
 
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("$baseUrl/swagger-ui/")
             .setViewName("forward:$baseUrl/swagger-ui/index.html")
     }
+
+
 
 }
